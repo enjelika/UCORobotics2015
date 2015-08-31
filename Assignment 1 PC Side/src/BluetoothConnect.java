f21941b904da75dcdace6a863b5203bbd876eff8
enjelika <enjelika@yahoo.com>
2015-08-31 17:41:27 -0500

    Sample PC Bluetooth connect code added. - Debra

--- Assignment 1 PC Side/src/BluetoothConnect.java
+++ Assignment 1 PC Side/src/BluetoothConnect.java
@@ -1,13 +1,71 @@
-import lejos.nxt.LCD;
-import lejos.pc.comm.*;
-import lejos.pc.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import lejos.pc.comm.NXTCommLogListener;
+import lejos.pc.comm.NXTConnector;
 
 public class BluetoothConnect {
 	
 	public static void main(String[] args) throws Exception{
 		
-		NXTComm nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.BLUETOOTH);
-		NXTInfo nxtInfo = new NXTInfo(NXTCommFactory.BLUETOOTH, "NXT", "");
+		NXTConnector conn = new NXTConnector();
 		
+		conn.addLogListener(new NXTCommLogListener()
+		{
+
+			public void logEvent(String message) {
+				System.out.println("BTSend Log.listener: "+message);
+				
+			}
+
+			public void logEvent(Throwable throwable) {
+				System.out.println("BTSend Log.listener - stack trace: ");
+				 throwable.printStackTrace();
+				
+			}
+		} );
+		
+		// Connect to any NXT over Bluetooth
+		boolean connected = conn.connectTo("btspp://");
+	
+		
+		if (!connected) {
+			System.err.println("Failed to connect to any NXT");
+			System.exit(1);
+		}
+		
+		DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+		DataInputStream dis = new DataInputStream(conn.getInputStream());
+				
+		for(int i=0;i<100;i++) {
+			try {
+				System.out.println("Sending " + (i*30000));
+				dos.writeInt((i*30000));
+				dos.flush();			
+				
+			} catch (IOException ioe) {
+				System.out.println("IO Exception writing bytes:");
+				System.out.println(ioe.getMessage());
+				break;
+			}
+			
+			try {
+				System.out.println("Received " + dis.readInt());
+			} catch (IOException ioe) {
+				System.out.println("IO Exception reading bytes:");
+				System.out.println(ioe.getMessage());
+				break;
+			}
+		}
+		
+		try {
+			dis.close();
+			dos.close();
+			conn.close();
+		} catch (IOException ioe) {
+			System.out.println("IOException closing connection:");
+			System.out.println(ioe.getMessage());
+		}
 	}
 }
